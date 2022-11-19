d1839a0f413b991bf872a06c5d6d84f4a4bfa86f
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-11-19 11:49:59 +0100

    added base for creating a quote without db

--- src/quotes/quotes.controller.ts
+++ src/quotes/quotes.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { QuotesService } from './quotes.service';
+import { CreateQuoteDto } from './dto/create-quote.dto';
 
 @Controller('quotes')
 export class QuotesController {
   constructor(private readonly quotesService: QuotesService) {}
+
+  @Get()
+  getAllQuotes() {
+    return this.quotesService.getAllQuotes();
+  }
+
+  @Get(':id')
+  getQuoteById(@Param('id') id: number) {
+    return this.quotesService.getQuoteById(id);
+  }
+
+  @Post()
+  createQuote(@Body() createQuoteDto: CreateQuoteDto) {
+    return this.quotesService.createQuote(createQuoteDto);
+  }
 }
--- src/quotes/quotes.service.ts
+++ src/quotes/quotes.service.ts
@@ -1,4 +1,40 @@
+import { CreateQuoteDto } from './dto/create-quote.dto';
 import { Injectable } from '@nestjs/common';
+import { Quote } from './quote.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class QuotesService {}
+export class QuotesService {
+  private readonly quotes: Quote[] = [];
+
+  getAllQuotes(): Quote[] {
+    return this.quotes;
+  }
+
+  getQuoteById(id: number): Quote {
+    return this.quotes.find((quote) => quote.id === id);
+  }
+
+  createQuote(createQuoteDto: CreateQuoteDto) {
+    const { content, user_id } = createQuoteDto;
+
+    const quote: Quote = {
+      id: uuid(),
+      content,
+      user_id,
+    };
+
+    this.quotes.push(quote);
+    return quote;
+  }
+
+  updateQuote(id: number, content: string) {
+    const index = this.quotes.findIndex((quote) => quote.id === id);
+    this.quotes[index].content = content;
+  }
+
+  deleteQuote(id: number) {
+    const index = this.quotes.findIndex((quote) => quote.id === id);
+    this.quotes.splice(index, 1);
+  }
+}
