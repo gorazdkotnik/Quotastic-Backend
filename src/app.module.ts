57cc36fd2d49d6491dbe2580135266886ef40ade
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-11-19 11:28:41 +0100

    added base quotes files for operating with requests

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { QuotesModule } from './quotes/quotes.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [QuotesModule],
 })
 export class AppModule {}
