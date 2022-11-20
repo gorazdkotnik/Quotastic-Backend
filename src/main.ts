b9b49cc58c8c90a74f4f762ce7928a24f72540f2
Gorazd Kotnik <59970689+gorazdkotnik@users.noreply.github.com>
2022-11-20 18:42:51 +0100

    added helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT');
+
+  app.use(helmet());
 
   await app.listen(port);
 }
